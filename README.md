5e820ac6cb6bb320aac2e9b3826091ed5d328b64
bsse24029 <bsse24029@itu.edu.pk>
2025-01-12 20:52:18 +0500

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# React Infinite Scroll
+
+A React application that shows how we can achieve "infinite scrolling" for our backend API. For implementation in shown way API must support limit or pagination.
+
+We are using a custom Hook to do fetching and taking care of the state. Intersection Observer is used to know when the last element is on the screen.
+ 
+ 
---
+++ app/src/App.js
@@ -0,0 +1,17 @@
+import { ChakraProvider } from "@chakra-ui/react";
+import List from "./components/List";
+import { Heading, AppContainer, Backdrop } from "./components/AppContainer";
+
+function App() {
+  return (
+    <ChakraProvider>
+      <Backdrop />
+      <AppContainer>
+        <Heading>Quotes to keep you going in your Life</Heading>
+        <List />
+      </AppContainer>
+    </ChakraProvider>
+  );
+}
+
+export default App;
